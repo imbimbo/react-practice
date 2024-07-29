7316662efc95bd45e4dcf63af65c7d9fa7648dc1
felipe imbimbo <f_imbimbo@hotmail.com>
2024-07-29 01:29:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header'
+import InputWithText from './InputWithText'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <InputWithText/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+return (
+<div className='header'>
+  <ul>
+    <li></li>
+  </ul>
+
+</div>
+)
+}
+
+export default Header
